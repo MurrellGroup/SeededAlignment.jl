17ba70768ceed8e3c229c7ea69e082412c2ba674
Anton Oresten <antonoresten@gmail.com>
2025-04-16 13:11:38 +0200

    add documentation deployment; update Project.toml, README.md, LICENSE

---
+++ docs/make.jl
@@ -0,0 +1,29 @@
+using SeededAlignment
+using Documenter
+
+DocMeta.setdocmeta!(
+    SeededAlignment,
+    :DocTestSetup,
+    :(using SeededAlignment);
+    recursive=true,
+)
+
+makedocs(;
+    modules=[SeededAlignment],
+    authors="Willó Corry and contributors",
+    sitename="SeededAlignment.jl",
+    format=Documenter.HTML(;
+        canonical="https://MurrellGroup.github.io/SeededAlignment.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+    doctest=false,
+)
+
+deploydocs(;
+    repo="github.com/MurrellGroup/SeededAlignment.jl",
+    devbranch="main",
+)
